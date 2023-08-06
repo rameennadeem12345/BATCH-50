125c8e96fc50676dbd81c05d9f07196bb321f423
rameennadeem12345 <141517153+rameennadeem12345@users.noreply.github.com>
2023-08-06 11:48:20 -0700

    Assignment no 1

---
+++ index.js
@@ -0,0 +1,24 @@
+"use string";
+console.log(" Hey Abu Hurairah , I have started learning TYPESCRIPT. ");
+var naMe=("rameen")
+console.log(naMe);    
+var num1=10;
+var num2=12;
+var num3=13;
+var num4=14;
+var num5=15;
+var num6=16;
+var num7=17;
+var num8=18;
+var num9=19;
+var num10=20;
+var Sum= num1 + num2 + num3 + num4 + num5 + num6 + num7 + num8 + num9 + num10;
+console.log("The Sum Is : " + Sum);
+var Sub= num1 - num2 - num3 - num4 - num5 - num6 - num7 - num8 - num9 - num10;
+console.log("The Subtraction Is : " + Sum);
+var Mul= num1 * num2 * num3 * num4 * num5 * num6 * num7 * num8 * num9 * num10;
+console.log("The Multiplication Is : " + Mul);
+var Div= num10 / num1;
+console.log("The Division Is : " + Div);
+var result= num1 + num2 * num3 + num4 / num5 + num6 + num7 + num8 + num9 + num10
+console.log("The Result Is : " + result)
---
+++ index.ts
@@ -0,0 +1,24 @@
+// Assignment NO#1
+console.log(" Hey Abu Hurairah , I have started learning TYPESCRIPT. ");
+var naMe:string =  ("rameen")
+console.log(naMe);    
+var num1:number = 10;
+var num2:number = 12;
+var num3:number = 13;
+var num4:number = 14;
+var num5:number = 15;
+var num6:number = 16;
+var num7:number = 17;
+var num8:number = 18;
+var num9:number = 19;
+var num10:number = 20;
+var Sum:number = num1 + num2 + num3 + num4 + num5 + num6 + num7 + num8 + num9 + num10;
+console.log("The Sum Is : " + Sum);
+var Sub:number = num1 - num2 - num3 - num4 - num5 - num6 - num7 - num8 - num9 - num10;
+console.log("The Subtraction Is : " + Sub);
+var Mul:number = num1 * num2 * num3 * num4 * num5 * num6 * num7 * num8 * num9 * num10;
+console.log("The Multiplication Is : " + Mul);
+var Div:number = num10 / num1;
+console.log("The Division Is : " + Div);
+var result:number = num1 + num2 * num3 + num4 / num5 + num6 + num7 + num8 + num9 + num10
+console.log("The Result Is : " + result)
